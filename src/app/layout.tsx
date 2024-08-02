981c0d717b386962962faca3ff19f573db6b2089
hossein.naseri <hossein.naseri.info@gmail.com>
2024-08-02 17:18:23 +0330

    feat(app): implement route protection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import ReactQueryProvider from "@/packages/system/providers/ReactQueryProvider/ReactQueryProvider";
 import { Toaster } from 'react-hot-toast';
+import { cookies } from 'next/headers'
 
 const inter = Inter({ subsets: ["latin"] });
 
